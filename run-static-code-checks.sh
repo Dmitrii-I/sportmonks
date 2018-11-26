5accc0f8fcede631433c4cf686b0e644ff888cc7
Dmitrii Izgurskii <izgurskii@gmail.com>
2018-11-26 22:14:23 +0100

    Run mypy using for loop
    Run mypy using for loop because it seems providing all files as N
    arguments to `mypy` command does not work as I expect.

--- run-static-code-checks.sh
+++ run-static-code-checks.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 echo "Run pylava"
-find -name '*.py' | xargs pylava --options setup.cfg --format pep8
+#find -name '*.py' | xargs pylava --options setup.cfg --format pep8
 
 echo "Run mypy"
-find -name '*.py' | xargs mypy
+readonly python_files=$(find -name '*.py')
+
+for f in $python_files; do
+    mypy --config-file setup.cfg $f
+done
 
