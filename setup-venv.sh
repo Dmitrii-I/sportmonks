d731f10889dba275c33ef08aa7150ff04cac6dda
Dmitrii-I <izgurskii@gmail.com>
2019-08-24 21:02:11 +0200

    Add script that sets up virtual environment

---
+++ setup-venv.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -o nounset      # exit with non-zero status if expansion is attempted on an unset variable
+set -o errexit      # exit immediatelly if a pipeline, a list, or a compound command fails
+set -o pipefail     # failures in pipe in the commands before last one, also count as failures
+
+# Trapping non-zero exit codes:
+on_error() {
+    line_num="$1"
+    echo "Caught error on line $line_num"
+}
+
+on_exit() {
+    true
+}
+
+on_interrupt() {
+    true
+}
+trap 'on_error $LINENO' ERR
+trap on_exit EXIT
+trap on_interrupt INT
+
+echo 'This script sets up a virtual environment in which `sportmonks` can be tested and developed'
+
+
+echo 'Create virtual environment in `venv` directory'
+python3 -m venv venv --copies
+
+echo 'Activate virtual environment'
+source venv/bin/activate
+
+echo 'Upgrade pip'
+pip install pip --upgrade
+
+echo 'Install from requirements.txt'
+pip install -r requirements.txt
+
+echo 'Check for broken requirements'
+pip check
+
