065963d559ae3aac9b627fb5f92d9bc8233b3f29
Dmitrii-I <izgurskii@gmail.com>
2019-09-07 11:50:43 +0200

    Set conservative bash options and trap errors

--- run-static-code-checks.sh
+++ run-static-code-checks.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+set -o nounset      # exit with non-zero status if expansion is attempted on an unset variable
+set -o errexit      # exit immediatelly if a pipeline, a list, or a compound command fails
+set -o pipefail     # failures in pipe in the commands before last one, also count as failures
+
+# Trapping non-zero exit codes:
+on_error() {
+    line_num="$1"
+    echo "Caught error on line $line_num"
+}
+
+on_exit() {
+    true
+}
+
+on_interrupt() {
+    true
+}
+trap 'on_error $LINENO' ERR
+trap on_exit EXIT
+trap on_interrupt INT
+
 echo "Run pylava"
 find -name '*.py' | xargs pylava --options setup.cfg --format pep8
 
