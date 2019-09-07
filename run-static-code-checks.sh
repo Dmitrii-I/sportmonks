8da28707943b41b2465c7b37cdce65fd01c49be7
Dmitrii-I <izgurskii@gmail.com>
2019-09-07 11:49:22 +0200

    Uncomment line that runs `pylava` tests

--- run-static-code-checks.sh
+++ run-static-code-checks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Run pylava"
-#find -name '*.py' | xargs pylava --options setup.cfg --format pep8
+find -name '*.py' | xargs pylava --options setup.cfg --format pep8
 
 echo "Run mypy"
 
