7bedc9a6cf7a0bc149efaa63790249f59cf5f28b
Dmitrii-I <izgurskii@gmail.com>
2019-11-05 07:11:03 +0100

    Use full paths instead of relative
    Use full path to call the `sphinx-build` utility inside venv.

--- docs/build.sh
+++ docs/build.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
+set -o nounset      # exit with non-zero status if expansion is attempted on an unset variable
+set -o errexit      # exit immediatelly if a pipeline, a list, or a compound command fails
+set -o pipefail     # failures in pipe in the commands before last one, also count as failures
+
+# Trapping non-zero exit codes:
+on_error() {
+    line_num="$1"
+    echo "Caught error on line $line_num"
+}
+
+on_exit() {
+    true
+}
+
+on_interrupt() {
+    true
+}
+trap 'on_error $LINENO' ERR
+trap on_exit EXIT
+trap on_interrupt INT
 
 echo Build documentation
-sphinx-build -b html ./ build/
+~/sportmonks/venv/bin/sphinx-build -b html ~/sportmonks/docs ~/sportmonks/docs/build/
 
