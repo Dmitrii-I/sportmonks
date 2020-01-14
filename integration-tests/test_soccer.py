31fcca6422b6e685b190396ff07acf29bfd9703f
Dmitrii-I <izgurskii@gmail.com>
2020-01-14 20:14:55 +0100

    Expect `player()` to return `display_name`

--- integration-tests/test_soccer.py
+++ integration-tests/test_soccer.py
@@ -899,6 +899,7 @@ def test_player(soccer_api):
         "position",
         "stats",
         "trophies",
+        "display_name"
     }
 
     actual = set(soccer_api.player(player_id=579, includes=["team", "position", "stats", "trophies"]).keys())
