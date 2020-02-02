3182c48fb1fd13ea28429097cf73d35dd124b548
Dmitrii-I <izgurskii@gmail.com>
2020-02-02 16:23:40 +0100

    Call `find` with full path
    Windows seems to use find.exe in the subshell, but we want it it use the
    Linux `find`. Maybe by providing full path to `find` it will work?

--- run-static-code-checks.sh
+++ run-static-code-checks.sh
@@ -28,7 +28,7 @@ set_environment_variables
 activate_virtual_environment
 
 echo "Test code"
-paths="$(find ~/sportmonks/sportmonks -name \"*.py\")"
+paths="$(/usr/bin/find ~/sportmonks/sportmonks -name '*.py')"
 for path in $paths; do
     echo
     echo "##############################################################################"
