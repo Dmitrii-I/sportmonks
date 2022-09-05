52bea6466d01d5f2f9ad5e1168160d16ca8756f1
Dmitrii-I <izgurskii@gmail.com>
2022-09-05 13:37:50 +0200

    Install setuptools, update pip, pip-tools
    Trying to fix the issue with the failing GitHub actions on Mac OS.

--- setup-venv.sh
+++ setup-venv.sh
@@ -2,7 +2,7 @@
 
 POSIXLY_CORRECT=1 set -o errexit && set -o nounset && set -o pipefail && unset POSIXLY_CORRECT
 
-echo "This script sets up a virtual environment in which `sportmonks` can be tested and developed"
+echo 'This script sets up a virtual environment in which `sportmonks` can be tested and developed'
 
 source ~/sportmonks/functions.sh
 set_environment_variables
@@ -20,13 +20,17 @@ activate_virtual_environment
 echo 'Upgrade pip'
 # On Windows you cannot replace a binary that is running, hence pip install pip --upgrade fails.
 # https://stackoverflow.com/questions/58627922/pip-install-upgrade-pip-fails-inside-a-windows-virtualenv-with-access-denie
-$PYTHON -m pip install pip==21.3.1
+$PYTHON -m pip install pip==22.2.2
 
 echo 'Install wheel'
 pip install wheel==0.37.1
 
+echo 'Install setuptools'
+pip install setuptools==65.3.0
+
+
 echo 'Install pip-tools'
-pip install pip-tools==6.4.0
+pip install pip-tools==6.8.0
 
 echo 'Install from requirements.txt'
 pip-sync ~/sportmonks/requirements-tests.txt
